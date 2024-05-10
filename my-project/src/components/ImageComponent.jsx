6e233aaaa42c4248ab8d60ae278ec53fffdc021b
Fahryzal <fahrjilo@gmail.com>
2024-05-10 19:06:59 +0700

    delete commit

--- my-project/src/components/ImageComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ImageComponent = ({ src, style }) => {
-  return <img src={src} className={style} />;
-};
-
-export default ImageComponent;
--- my-project/src/components/TitleProduct.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TitleProduct = ({ children }) => {
-  return <div className="font-cf text-3xl tracking-[3px]">{children}</div>;
-};
-
-export default TitleProduct;
